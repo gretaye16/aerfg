7fc0426befcb1687fdeb4b1aea0bac32d911f22a
gretaye16 <90076991+gretaye16@users.noreply.github.com>
2022-06-28 21:04:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function modelLoaded() {
 function gotPoses() {
 
     if (results.length>0) {
-        console.log(rseults); 
+        console.log(results); 
         noseX = results[0].pose.nose.x;
         noseY = results[0].pose.nose.y;
         console.log("noseX = " + noseX + ", noseY = " + noseY);
